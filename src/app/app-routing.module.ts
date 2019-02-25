dd6d489ee49c5d2f71770c8b7af13decdd5cd91f
Kusal Kankanamge <kusald.14@cse.mrt.ac.lk>
2019-02-25 21:45:40 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SignUpComponent} from './sign-up-component/sign-up.component';
+import {IndexComponent} from './index-component/index.component';
+import {SignInComponent} from './sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'singin', pathMatch:'full'},
+  {path: 'signin', component: SignInComponent},
+  {path: 'signup', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {IndexComponent} from './index-component/index.component';
+import { SignUpComponent } from './sign-up-component/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        IndexComponent,
+        SignUpComponent,
+        SignInComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
